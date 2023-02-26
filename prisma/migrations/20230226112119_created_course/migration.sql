a5aa2ca562ae57760d558bd165fc37dee1adc88d
HaiderMalik12 <haidermalik504@gmail.com>
2023-02-26 16:34:32 +0500

    created course model

---
+++ prisma/migrations/20230226112119_created_course/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "desc" TEXT NOT NULL,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
