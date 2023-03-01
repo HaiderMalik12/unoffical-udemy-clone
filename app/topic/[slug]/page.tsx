99e158b668c0f14d21df203539154c1fa83937f0
HaiderMalik12 <haidermalik504@gmail.com>
2023-03-01 23:04:49 +0500

    plan to fetch courses on the based on topic

--- app/topic/[slug]/page.tsx
+++ app/topic/[slug]/page.tsx
@@ -1,5 +1,11 @@
 import Courses from '@/components/Courses';
 
 export default function Topic() {
+  // create getData method
+  // you must need to get the slug
+  // fetch the courses on the based on slug
+  // you need to provide the courses array
+  // the courses you have fetched from the backend api
+  // then pass it to courses
   return <Courses />;
 }
