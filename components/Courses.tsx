7a80cbf32e06595fdc24f56f1e7e7d7b5b2169da
HaiderMalik12 <haidermalik504@gmail.com>
2023-03-05 15:04:21 +0500

    update card

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -22,44 +22,40 @@ export function Courses() {
           </div>
         </div>
         <div className="col">
-          <div className="col">
-            <div className="card">
-              <img
-                className="card-img-top"
-                src="/images/courses/sample.svg"
-                alt="Card image cap"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
+          <div className="card">
+            <img
+              className="card-img-top"
+              src="/images/courses/sample.svg"
+              alt="Card image cap"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
             </div>
           </div>
         </div>
         <div className="col">
-          <div className="col">
-            <div className="card">
-              <img
-                className="card-img-top"
-                src="/images/courses/sample.svg"
-                alt="Card image cap"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
+          <div className="card">
+            <img
+              className="card-img-top"
+              src="/images/courses/sample.svg"
+              alt="Card image cap"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
             </div>
           </div>
         </div>
